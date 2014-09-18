2eda787a9b606cab9e10af56eb9d7dc821c490b4
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2014-09-18 18:27:50 +0900

    add some basic classes

---
+++ src/EzAction.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+/**
+ * the EzAction Object is only used to store the information of action, which have been done.
+ * and affected EzTask Objects. 
+ * targets is the list of tasks before doing the action
+ * results is the list of task after the action
+ * 
+ * for ADD action, targets is NULL and results is an 1-element array of task.
+ * for UPDATE action, targets and results are the list of task before and after doing the action respectively.
+ * for DELETE action, targets is the list of tasks that will be delete while results is NULL.
+ * for DONE action, it is the same as UPDATE. 
+ */
+
+/**
+ * @author Khanh
+ *
+ */
+
+public class EzAction {
+	enum TypeOfAction{
+		ADD, UPDATE, DELETE, DONE
+	}
+	
+	private TypeOfAction action;
+	private ArrayList<EzTask> targets;
+	private ArrayList<EzTask> results;
+	
+	/**
+	 * @return the action
+	 */
+	public TypeOfAction getAction() {
+		return action;
+	}
+	/**
+	 * @param action the action to set
+	 */
+	public void setAction(TypeOfAction action) {
+		this.action = action;
+	}
+	/**
+	 * @return the targets
+	 */
+	
+	public ArrayList<EzTask> getTargets() {
+		return targets;
+	}
+	/**
+	 * @param targets the targets to set
+	 */
+	public void setTargets(ArrayList<EzTask> targets) {
+		this.targets = targets;
+	}
+	
+	/**
+	 * @return the results
+	 */
+	public ArrayList<EzTask> getResults() {
+		return results;
+	}
+	/**
+	 * @param results the results to set
+	 */
+	public void setResults(ArrayList<EzTask> results) {
+		this.results = results;
+	}
+	
+	
+}
---
+++ src/EzTask.java
@@ -0,0 +1,180 @@
+/**
+ * Task Object
+ */
+
+/**
+ * @author Khanh
+ *
+ */
+import java.util.*;
+
+public class EzTask {
+	private int id;
+	private String title;
+	private Date startTime;
+	private Date endTime;
+	private String venue;
+	private int priority;
+	
+	
+	/**
+	 * initialize by other EzTask Object
+	 * @param target
+	 */
+	public EzTask(EzTask target){
+		this.id = target.getId();
+		this.title = new String(target.getTitle());
+		this.startTime = new Date(target.getStartTime().getTime());
+		this.endTime = new Date(target.getEndTime().getTime());
+		this.venue = new String(target.getVenue());
+		this.priority = target.getPriority();
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	/**
+	 * get the Date from 3 inputs
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @return Date
+	 */
+	private Date getTime(int year, int month, int day){
+		Calendar cal = new GregorianCalendar(year,month,day);
+		return cal.getTime();
+	}
+	
+	/**
+	 * get the Date from 5 inputs
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @param hour
+	 * @param minute
+	 * @return Date
+	 */
+	private Date getTime(int year, int month, int day,int hour,int minute){
+		Calendar cal = new GregorianCalendar(year,month,day,hour,minute);
+		return cal.getTime();
+	}
+	
+	/**
+	 * @return the startTime
+	 */
+	public Date getStartTime() {
+		return startTime;
+	}
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	
+	/**
+	 * set startTime by 3 input
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public void setStartTime(int year, int month, int day) {
+		this.startTime = getTime(year,month,day);
+	}
+	
+	/**
+	 * set startTime by 5 input
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @param hour
+	 * @param minute
+	 */
+	public void setStartTime(int year, int month, int day,int hour,int minute) {
+		this.startTime = getTime(year,month,day,hour,minute);
+	}
+	
+	/**
+	 * @return the endTime
+	 */
+	public Date getEndTime() {
+		return endTime;
+	}
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	
+	/**
+	 * set endTime by 3 inputs
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public void setEndTime(int year, int month, int day) {
+		this.endTime = getTime(year,month,day);
+	}
+	
+	/**
+	 * set endTime by 5 inputs
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @param hour
+	 * @param minute
+	 */
+	public void setEndTime(int year, int month, int day,int hour,int minute) {
+		this.endTime = getTime(year,month,day,hour,minute);
+	}
+	
+	/**
+	 * @return the venue
+	 */
+	public String getVenue() {
+		return venue;
+	}
+	/**
+	 * @param venue the venue to set
+	 */
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+	
+	/**
+	 * @return the priority
+	 */
+	public int getPriority() {
+		return priority;
+	}
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+}
