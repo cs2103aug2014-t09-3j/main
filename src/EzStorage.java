2a89c0f148ac3152b0dbe3153d52ce1d108622ab
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2014-09-20 22:55:14 +0900

    Delete some functions in EzStorage

--- src/EzStorage.java
+++ src/EzStorage.java
@@ -21,60 +21,12 @@ public class EzStorage {
 		return null;
 	}
 	
-	
-	/**
-	 * this function creates a copy of the task then changes the title of the copy into title and returns the copy.
-	 * @param task
-	 * @param title
-	 * @return the copy of the task with a new title
-	 */
-	public EzTask editTitle(EzTask task, String title){
-		return null;
-	}
-	
-	/**
-	 * this function creates a copy of the task then changes the date of the copy into date and returns the copy.
-	 * @param task
-	 * @param date
-	 * @return the copy of the task with a new date
-	 * 
-	 * Note: Date here includes both date and time
-	 * 
-	 */
-	public EzTask editDate(EzTask task, Date date){
-		return null;
-	}
-	
-	/**
-	 * this function creates a copy of the task then changes the priority of the copy into priority and returns the copy.
-	 * @param task
-	 * @param priority
-	 * @return the copy of the task with a new priority
-	 */
-	public EzTask editPriority(EzTask task, int priority){
-		return null;
-	}
-	
-	/**
-	 * this function creates a copy of the task then changes the venue of the copy into venue and returns the copy.
-	 * @param task
-	 * @param venue
-	 * @return the copy of the task with a new venue
-	 */
-	public EzTask editVenue(EzTask task, String venue){
-		return null;
-	}
-	
 	/**
 	 * this function creates a copy of the listOfTasks then marks all tasks in the copy as Done and return the copy.
 	 * @param listOfTasks
 	 * @return
 	 */
 	public ArrayList<EzTask> updateTask(ArrayList<EzTask> oriTask) {
-		return null;
-	}
-	
-	public ArrayList<EzTask> markDone(ArrayList<EzTask> listOfTasks){ 
 		return null;
 	}
 	
