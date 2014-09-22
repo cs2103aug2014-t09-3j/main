2bcc24d468a625e1572a83c4164e44c643cf0aba
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2014-09-22 23:30:51 +0900

    add format of ADD command

--- src/EzParser.java
+++ src/EzParser.java
@@ -1,7 +1,28 @@
 import java.util.*;
 
 /**
- * 
+ * COMMAND:
+	ADD "title"			: set the title of EzTask object
+		AT "venue"		: set the venue of EzTask object
+		AT time			: set the time 		of both startTime and endTime of EzTask object
+		AT time	date	: set the date and time of both startTime and endTime of EzTask object
+		ON date			: set the date 		of both startTime and endTime of EzTask object
+		ON date time	: set the date and time of both startTime and endTime of EzTask object
+		TODAY			: if it appears after ON, FROM or TO, it represents the date of today
+				  			if it stands alone, the same as ON TODAY
+		TOMORROW		: if it appears after ON, FROM or TO, it represents the date of tomorrow
+				  			if it stands alone, the same as ON TOMORROW
+		****			: set the priority depending on the number of *
+		FROM date		: set the date 		of startTime of EzTask object
+		FROM time		: set the time 		of startTime of EzTask object
+		FROM date time	: set the date & time 	of startTime of EzTask object
+		FROM time date 	: set the date & time 	of startTime of EzTask object
+		TO date			: set the date 		of endTime of EzTask object
+		TO time			: set the time 		of endTime of EzTask object
+		TO date time	: set the date & time 	of endTime of EzTask object
+		TO time date 	: set the date & time 	of endTime of EzTask object
+		
+
  */
 
 /**
