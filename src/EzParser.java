3447973d37e7ad62d5b71e2050c8d7ede7280d65
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2014-09-26 17:23:12 +0900

    comments for parser

--- src/EzParser.java
+++ src/EzParser.java
@@ -21,7 +21,57 @@ import java.util.*;
 		TO time			: set the time 		of endTime of EzTask object
 		TO date time	: set the date & time 	of endTime of EzTask object
 		TO time date 	: set the date & time 	of endTime of EzTask object
+		COMMAND:
+ADD "title"		: set the title of EzTask object
+	AT "venue"	: set the venue of EzTask object
+	AT time		: set the time 		of both startTime and endTime of EzTask object
+	AT time	date	: set the date and time of both startTime and endTime of EzTask object
+	ON date		: set the date 		of both startTime and endTime of EzTask object
+	ON date time	: set the date and time of both startTime and endTime of EzTask object
+	TODAY		: if it appears after ON, FROM or TO, it represents the date of today
+			  if it stands alone, the same as ON TODAY
+	TOMORROW	: if it appears after ON, FROM or TO, it represents the date of tomorrow
+			  if it stands alone, the same as ON TOMORROW
+	****		: set the priority depending on the number of *
+	FROM date	: set the date 		of startTime of EzTask object
+	FROM time	: set the time 		of startTime of EzTask object
+	FROM date time	: set the date & time 	of startTime of EzTask object
+	FROM time date 	: set the date & time 	of startTime of EzTask object
+	TO date		: set the date 		of endTime of EzTask object
+	TO time		: set the time 		of endTime of EzTask object
+	TO date time	: set the date & time 	of endTime of EzTask object
+	TO time date 	: set the date & time 	of endTime of EzTask object
 		
+
+UPDATE id 
+	SET TITLE "newTitle"
+	SET DATE [newDate]
+	SET TIME [newTime]
+	SET START DATE [newDate]
+	SET START TIME [newTime]
+	SET END DATE [newDate]
+	SET END TIME [newTime]
+	SET VENUE "newVenue"
+	SET PRIORITY [newPriority]
+
+UNDO
+REDO
+
+DONE [id1] [id2] [id3]...
+DONE FROM [startID] TO [endID]
+DONE ALL
+	ON [date]
+
+DELETE [id1] [id2] [id3]
+DELETE FROM [startId] TO [endId]
+DELETE ALL
+	ON [date]
+
+SHOW ALL
+SHOW DONE
+SHOW UNDONE
+	ON [date]
+	HAVE [keyword1] [keyword2]...
 
  */
 
