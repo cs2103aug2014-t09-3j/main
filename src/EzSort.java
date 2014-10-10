c7d40fecd2c3bc477dfbb8c0c4c92f86c85efcbc
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2014-10-10 10:07:22 +0900

    add EzSort

---
+++ src/EzSort.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author Khanh
+ *
+ */
+public class EzSort {
+	
+	/**
+	 * return the target sorted by Id
+	 * @param target
+	 * @return
+	 */
+	public static ArrayList<EzTask> sortById(ArrayList<EzTask> target){
+		
+	}
+	
+	/**
+	 * return the target sorted by start time
+	 * @param target
+	 * @return
+	 */
+	public static ArrayList<EzTask> sortByDate(ArrayList<EzTask> target){
+		
+	}
+	
+	/**
+	 * return the target sorted by Priority
+	 * @param target
+	 * @return
+	 */
+	public static ArrayList<EzTask> sortByPriority(ArrayList<EzTask> target){
+		
+	}
+	
+	/**
+	 * return the target sorted by title
+	 * @param target
+	 * @return
+	 */
+	public static ArrayList<EzTask> sortByTitle(ArrayList<EzTask> target){
+		
+	}
+	
+	/**
+	 * return the target sorted by venue
+	 * @param target
+	 * @return
+	 */
+	public static ArrayList<EzTask> sortByVenue(ArrayList<EzTask> target){
+		
+	}
+	
+	/**
+	 * return the target sorted by done
+	 * Undone tasks come first
+	 * 
+	 * @param target
+	 * @return
+	 */
+	public static ArrayList<EzTask> sortByDone(ArrayList<EzTask> target){
+		
+	}
+	
+	
+}
